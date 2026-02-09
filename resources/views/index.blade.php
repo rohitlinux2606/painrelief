cc02af49fb987cd14254050837e7ac7e48538313
Rohit <rohit99336@gmail.com>
2026-02-09 16:38:43 +0530

    change banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -389,7 +389,7 @@
     </nav>
 
     <section class="banner-section">
-        <img src="{{ asset('cdn/001.png') }}" alt="Joint Pain Relief Banner">
+        <img src="{{ asset('banner/flipkart.jpeg') }}" alt="Joint Pain Relief Banner">
     </section>
 
     <section class="container py-5">
