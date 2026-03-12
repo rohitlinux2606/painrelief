154100e6852f324270f9320a117912bc45ea2db7
Rohit <rohit99336@gmail.com>
2026-03-12 15:04:34 +0530

    feat: remove Facebook Meta Pixel tracking code from web layout

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -32,33 +32,6 @@
     <meta name="msapplication-TileImage" content="{{ asset('fevicon/ms-icon-144x144.png') }}">
     <meta name="theme-color" content="#1a4d2e">
 
-    <!-- Meta Pixel Code -->
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '3493740337444036');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=3493740337444036&ev=PageView&noscript=1" /></noscript>
-    <!-- End Meta Pixel Code -->
-
     <style>
         :root {
             --primary-green: #1a4d2e;
