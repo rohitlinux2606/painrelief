e3b4d4bc22a510107bff5601fa4451548314b8f8
Rohit <rohit99336@gmail.com>
2026-03-13 20:30:46 +0530

    refactor: comment out the "Add & Checkout" button link for products.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -419,7 +419,7 @@
                             {{-- <a href="{{ route('add-to-cart', $product->id) }}" class="btn-custom btn-atc"
                                 onclick="addToCartEvent()">Add & Checkout</a> --}}
 
-                            <a href="{{ $product->external_link }}" class="btn-custom btn-atc">Add & Checkout</a>
+                            {{-- <a href="{{ $product->external_link }}" class="btn-custom btn-atc">Add & Checkout</a> --}}
                             <a href="{{ $product->external_link }}" target="_blank" class="btn-custom btn-buy"
                                 onclick="buyNowEvent()">Buy
                                 Now</a>
