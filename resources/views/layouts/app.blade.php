f4c85cd185a9d5fe2d15058ba0c57f738b2dd096
Rohit <rohit99336@gmail.com>
2026-03-14 14:51:28 +0530

    refactor: remove `config('app.name')` from the default title fallback in the app layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', config('app.name', 'Vatahari'))</title>
+    <title>@yield('title', 'Vatahari')</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
