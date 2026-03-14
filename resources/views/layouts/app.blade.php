b5c013dcb47b88a37d19ac4f8f09e8707f8e7cd6
Rohit <rohit99336@gmail.com>
2026-03-14 14:26:32 +0530

    change pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
         }(window, document, 'script',
             'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '1940729423378921');
-        // fbq('track', 'PageView');
+        fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
             src="https://www.facebook.com/tr?id=1940729423378921&ev=PageView&noscript=1" /></noscript>
